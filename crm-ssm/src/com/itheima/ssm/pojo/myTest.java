8437e103979cf014697d1039d05b8fdd8698e1a6
amdin <yu15011443007@126.com>
2017-12-15 22:32:25 +0800

    再次修改 myTest

--- crm-ssm/src/com/itheima/ssm/pojo/myTest.java
+++ crm-ssm/src/com/itheima/ssm/pojo/myTest.java
@@ -3,6 +3,16 @@ package com.itheima.ssm.pojo;
 public class myTest {
 
 	private String id;
+	private String name;
+	
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 
 	public String getId() {
 		return id;
