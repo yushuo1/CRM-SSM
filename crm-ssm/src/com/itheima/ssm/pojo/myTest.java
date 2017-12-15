577b086adf583a988bbe6fe45cac90d9e80d0f51
amdin <yu15011443007@126.com>
2017-12-15 22:31:48 +0800

    添加 mytest

---
+++ crm-ssm/src/com/itheima/ssm/pojo/myTest.java
@@ -0,0 +1,16 @@
+package com.itheima.ssm.pojo;
+
+public class myTest {
+
+	private String id;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	
+}
